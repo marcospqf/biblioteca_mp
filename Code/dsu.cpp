7ba43dcb2fef217732860a9dce606fa7f42c285b
marcospqf <marcosp.bh95@hotmail.com>
2017-03-06 01:07:29 -0300

    add 2-sat

--- Code/dsu.cpp
+++ Code/dsu.cpp
@@ -5,19 +5,19 @@ void init()
   memset(Rank,0,sizeof(Rank));
   for(int i=0;i<N;i++) p[i]=i;
 }
-int findset(int i)
+int findSet(int i)
 {
   if(p[i]==i) return i;
-  return p[i]=findset(p[i]);
+  return p[i]=findSet(p[i]);
 }
-bool same(int i, int j)
+bool sameSet(int i, int j)
 {
-  return (findset(i) == findset(j));
+  return (findSet(i) == findSet(j));
 }
 void unionSet(int i, int j)
 {
-  if (!same(i, j)) {
-    int x = findset(i), y=findset(j);
+  if (!sameSet(i, j)) {
+    int x = findSet(i), y=findSet(j);
   if (Rank[x] > Rank[y])
       p[y] = x;
     else {
